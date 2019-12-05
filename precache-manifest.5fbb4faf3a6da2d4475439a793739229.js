b24e11c3160ac678b66c517dc389f43350b41c15
Simon Parkyn <s.a.j.parkyn@brighton.ac.uk>
2019-12-05 14:47:16 +0000

    Updates

---
+++ precache-manifest.5fbb4faf3a6da2d4475439a793739229.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f4a5312e09bfc8e5299847f5207ace1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "fcb244460944eb6950fc",
+    "url": "/monsters-rolodex/static/css/main.2bdbe983.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fcb244460944eb6950fc",
+    "url": "/monsters-rolodex/static/js/main.affaa69b.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
